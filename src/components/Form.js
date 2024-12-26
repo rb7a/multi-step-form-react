f53344f0f9d10457b1ce8f7ad0e02ecf478ec2d5
rb7a <130118715+rb7a@users.noreply.github.com>
2024-12-27 02:34:12 +0800

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -32,7 +32,7 @@ function Form() {
     <div className="form">
       <div className="progressbar">
         <div
-          style={{ width: page === 0 ? "33.3%" : page == 1 ? "66.6%" : "100%" }}
+          style={{ width: page === 0 ? "33.3%" : page === 1 ? "66.6%" : "100%" }}
         ></div>
       </div>
       <div className="form-container">
